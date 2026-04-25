328bbe4e577b530bd031a000a463b7b970c013f9
aslam-mansuri-2002 <dholkadholka56@gmail.com>
2026-04-25 16:26:10 +0530

    add some qu.ans..

--- python_programs.py
+++ python_programs.py
@@ -3,3 +3,8 @@ print("Hello")
 
 # Write a Python program to do arithmetical operations addition and division.
 
+num_1 = float(input("Enter frist number :- "))
+num_2 = float(input("Enter seconde number :- "))
+sum = num_1 + num_2
+print(sum)
+
