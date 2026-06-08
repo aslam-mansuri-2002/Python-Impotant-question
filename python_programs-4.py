85797051dcd357cc2a2519c37e64a206c39a2700
aslam-mansuri-2002 <dholkadholka56@gmail.com>
2026-06-08 11:10:09 +0530

    question 138 clear

--- python_programs-4.py
+++ python_programs-4.py
@@ -356,3 +356,139 @@ def mean(num):
 mean(42)
 mean(12345)
 
+# --> 132 Create a function that takes an integer and returns a list from 1 to the given number,
+# where:
+'''1. If the number can be divided evenly by 4, amplify it by 10 (i.e. return 10 times the
+number).
+2. If the number cannot be divided evenly by 4, simply return the number.
+Examples
+amplify(4) ➞ [1, 2, 3, 40]
+amplify(3) ➞ [1, 2, 3]
+amplify(25) ➞ [1, 2, 3, 40, 5, 6, 7, 80, 9, 10, 11, 120, 13, 14, 15, 160, 17, 18, 19, 200, 21,
+22, 23, 240, 25]
+Notes
+The given integer will always be equal to or greater than 1.
+Include the number (see example above).
+To perform this problem with its intended purpose, try doing it with list
+comprehensions. If that's too difficult, just solve the challenge any way you can.'''
+
+def amplify(num):
+    return [i*10 if i%4==0 else i for i in range(1,num+1)]
+
+amplify(25)
+amplify(4)
+
+# --> 133 Create a function that takes a list of numbers and return the number that's unique.
+'''Examples
+unique([3, 3, 3, 7, 3, 3]) ➞ 7
+unique([0, 0, 0.77, 0, 0]) ➞ 0.77
+unique([0, 1, 1, 1, 1, 1, 1, 1]) ➞ 0  
+Notes
+Test cases will always have exactly one unique number while all others are the same.'''
+
+def unique(lst):
+    return [x for x in lst if lst.count(x)==1][0]
+
+unique([3,3,3,7,3])
+unique([0,0,0.77,0,0])
+
+# ---> Program 134
+'''Your task is to create a Circle constructor that creates a circle with a radius provided
+by an argument. The circles constructed must have two getters getArea() (PIr^2) and
+getPerimeter() (2PI*r) which give both respective areas and perimeter
+(circumference).
+For help with this class, I have provided you with a Rectangle constructor which you
+can use as a base example.
+Examples
+circy = Circle(11)
+circy.getArea()
+Should return 380.132711084365
+circy = Circle(4.44)
+circy.getPerimeter()
+Should return 27.897342763877365
+Notes
+Round results up to the nearest integer.'''
+
+import math
+class Circal:
+    def __init__(self,r):
+        self.r = r
+    def getArea(self):
+        result = math.pi * (self.r**2)
+        return round(result,3)
+    def getPrimeter(self):
+        result = 2*math.pi*self.r
+        return round(result,3)
+
+c = Circal(11)
+c.getArea()
+
+# -->  135 Create a function that takes a list of strings and return a list, sorted from shortest to
+# longest.
+'''Examples
+sort_by_length(["Google", "Apple", "Microsoft"]) ➞ ["Apple", "Google", "Microsoft"]
+sort_by_length(["Leonardo", "Michelangelo", "Raphael", "Donatello"]) ➞ ["Raphael","Leonardo", "Donatello", "Michelangelo"]
+sort_by_length(["Turing", "Einstein", "Jung"]) ➞ ["Jung", "Turing", "Einstein"]
+Notes
+All test cases contain lists with strings of different lengths, so you won't have to deal'''
+
+def sort_by_length(lst):
+    return sorted(lst,key=len)
+
+sort_by_length(["google","apple","microsoft"])
+
+# -->136 Create a function that validates whether three given integers form a Pythagorean
+# triplet. The sum of the squares of the two smallest integers must equal the square of
+# the largest number to be validated
+'''Examples
+is_triplet(3, 4, 5) ➞ True 
+3² + 4² = 25
+5² = 25
+is_triplet(13, 5, 12) ➞ True
+5² + 12² = 169
+13² = 169
+is_triplet(1, 2, 3) ➞ False
+1² + 2² = 5
+3² = 9
+Notes
+Numbers may not be given in a sorted order'''
+
+def is_triplet(a,b,c):
+    n = sorted([a,b,c])
+    return (n[0]**2)+(n[1]**2) == (n[2]**2)
+
+is_triplet(13,5,12)
+is_triplet(1,2,3)
+
+# --> 137 Create a function that takes three integer arguments (a, b, c) and returns the amount
+# of integers which are of equal value
+'''Examples
+equal(3, 4, 3) ➞ 2
+equal(1, 1, 1) ➞ 3
+equal(3, 4, 1) ➞ 0
+Notes
+Your function must return 0, 2 or 3.'''
+
+def equal(a,b,c):
+    if a==b==c:
+        return 3
+    elif a==b or b==c or a==c:
+        return 2
+    else:
+        return 0
+
+equal(3,2,3)
+equal(1,1,1)
+
+# --> 138 Write a function that converts a dictionary into a list of keys-values tuples
+'''Examples
+dict_to_list({"D": 1,"B": 2"C": 3}) ➞ [("B", 2), ("C", 3), ("D", 1)]
+dict_to_list({"likes": 2,"dislikes": 3,"followers": 10}) ➞ [("dislikes", 3), ("followers", 10), ("likes", 2)]
+Notes
+Return the elements in the list in alphabetical order.'''
+
+def dict_to_list(d):
+    return sorted(d.items())
+
+dict_to_list({"d":1,"b":2,"c":3})
+
